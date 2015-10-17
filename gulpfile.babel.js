d684aa1e92bb30f71c2bc6876750a62da0e31089
Adam Wills <heavymetaladam@gmail.com>
2015-10-16 22:48:45 -0400

    using del instead of clean

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -166,6 +166,5 @@ gulp.task('default', ['clean'], () => {
 
 gulp.task('deploy', ['build'], () => {
   return gulp.src('dist')
-    .pipe($.subtree())
-    .pipe($.clean());
+    .pipe($.subtree(del));
 });
